9a34130b17bb129e8ffbcb1570ea8a4b353332a0
Alexandre Araujo <10189140+alexandrearaujo-tech@users.noreply.github.com>
2024-03-23 18:31:02 +0000

    React working

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  # config/routes.rb
+  root to: redirect('/frontend/index.html')
+
+  # get "frontend(/*path)" => "frontend#index", as: frontend
 
   # Defines the root path route ("/")
   # root "posts#index"
